41a431b8ffe5b11ea0b4653d40aa96bbb82241df
rajan <rajanshahi1267@gmail.com>
2023-12-30 10:15:54 -0700

    add

--- src/pages/AddAdmins.jsx
+++ src/pages/AddAdmins.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const AddAdmins = () => {
+  const [creat, setcreat] = useState();
   const [passwordShow, setpasswordShow] = useState(false);
   const TogglePassword = () => {
     setpasswordShow(!passwordShow);
@@ -10,24 +12,34 @@ const AddAdmins = () => {
   const [email, setemail] = useState();
   const [password, setpassword] = useState();
 
-  const createAdmins = async () => {
-    const respones = await axios.post("http://localhost:4000/api/admins/register", {
-      email: email,
-      password: password,
-    });
+  const createAdmins = async (e) => {
+    e.preventDefault();
+    setcreat(true);
+
+    const respones = await axios.post(
+      "http://localhost:4000/api/admins/register",
+      {
+        email: email,
+        password: password,
+      }
+    );
+    setcreat(false);
     console.log(respones);
+    toast.success("Created Success");
+    setemail("");
+    setpassword("");
   };
-  
 
   return (
-    <div className=" flex flex-col gap-4 py-10 px-10">
+    <form onSubmit={createAdmins} className=" flex flex-col gap-4 py-10 px-10">
       <input
         value={email}
         onChange={(e) => setemail(e.target.value)}
         className="  px-2 border  border-blue-400 py-3 "
-        type="emial"
+        type="email"
         name="email"
         placeholder="Enter Your Email"
+        required
       />
       <div className=" w-full">
         <input
@@ -37,6 +49,7 @@ const AddAdmins = () => {
           className=" w-full  px-2 border border-blue-400 py-3 "
           type={passwordShow ? "text" : "password"}
           placeholder="Enter Your password"
+          required
         />{" "}
         <input
           onClick={TogglePassword}
@@ -47,10 +60,10 @@ const AddAdmins = () => {
           Password Show
         </h1>
       </div>
-      <button onClick={createAdmins} className=" bg-blue-500 text-white py-2">
-        create
+      <button type="submit" className=" bg-blue-500 text-white py-2">
+        {creat ? "creating" : "create"}
       </button>
-    </div>
+    </form>
   );
 };
 
