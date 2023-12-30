0090f303e5c02b6a44b8fd7994c311ef11af257e
rajan <rajanshahi1267@gmail.com>
2023-12-30 10:46:14 -0700

    add faqs page

--- src/pages/AddTestmoniorial.jsx
+++ src/pages/AddTestmoniorial.jsx
@@ -3,11 +3,13 @@ import React, { useState } from "react";
 import toast from "react-hot-toast";
 
 const AddTestmoniorial = () => {
+
   const [name, setname] = useState("");
   const [description, setdescription] = useState("");
-
   const [creating, setcreating] = useState(false);
-  const createstmonial = async () => {
+
+  const createstmonial = async (e) => {
+    e.preventDefault();
     setcreating(true);
     const response = await axios.post(
       "http://localhost:4000/api/testimonials",
@@ -25,13 +27,14 @@ const AddTestmoniorial = () => {
   console.log(creating);
 
   return (
-    <div className=" flex flex-col gap-4 py-10 px-10">
+    <form onSubmit={createstmonial} className=" flex flex-col gap-4 py-10 px-10">
       <input
         className="  px-2 border  border-blue-400 py-3 "
         type="text"
         value={name}
         onChange={(e) => setname(e.target.value)}
         placeholder="Enter Your Name"
+        required
       />
       <input
         value={description}
@@ -39,11 +42,12 @@ const AddTestmoniorial = () => {
         className=" px-2 border  border-blue-400 py-3 "
         type="text"
         placeholder="Enter Description"
+        required
       />
-      <button onClick={createstmonial} className=" bg-blue-500 text-white py-2">
+      <button type="submit" className=" bg-blue-500 text-white py-2">
         {creating ? "posting.." : "Create"}
       </button>
-    </div>
+    </form>
   );
 };
 
