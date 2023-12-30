b6507fb48288a89b7bb0cab060f37cbcacc5a636
rajan <rajanshahi1267@gmail.com>
2023-12-30 10:39:25 -0700

    addd

--- src/pages/AddFaqs.jsx
+++ src/pages/AddFaqs.jsx
@@ -7,28 +7,31 @@ const AddFaqs = () => {
   const [answer, setAnswer] = useState("");
   const [deleting, setdeleting] = useState();
 
-  const createFaqs = async () => {
-    setdeleting(true)
+  const createFaqs = async (e) => {
+    e.preventDefault();
+    setdeleting(true);
     const response = await axios.post("http://localhost:4000/api/faqs", {
       question: question,
       answer: answer,
     });
-  {  
-    response.status === 201 && toast.success(response.data.msg);}
+    {
+      response.status === 201 && toast.success(response.data.msg);
+    }
     console.log(response);
-    setdeleting(false)
-    setQuestion("")
-    setAnswer("")
+    setdeleting(false);
+    setQuestion("");
+    setAnswer("");
   };
 
   return (
-    <div className=" flex flex-col gap-4 py-10 px-10">
+    <form onSubmit={createFaqs} className=" flex flex-col gap-4 py-10 px-10">
       <input
         value={question}
         onChange={(e) => setQuestion(e.target.value)}
         className="  px-2 border  border-blue-400 py-3 "
         type="text"
         placeholder="Enter Your Question"
+        required
       />
       <input
         value={answer}
@@ -36,11 +39,12 @@ const AddFaqs = () => {
         className="  px-2 border border-blue-400 py-3 "
         type="text"
         placeholder="Enter Your Answer"
+        required
       />
-      <button onClick={createFaqs} className=" bg-blue-500 text-white py-2">
-       {deleting ? "Creating" :"create"}
+      <button type="submit" className=" bg-blue-500 text-white py-2">
+        {deleting ? "Creating" : "create"}
       </button>
-    </div>
+    </form>
   );
 };
 
