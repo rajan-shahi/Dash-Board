f6de9b7dadeff278064cb6d1eee724d70d1a0940
rajan <rajanshahi1267@gmail.com>
2024-01-08 04:37:29 -0700

    add navbar committees page

--- src/pages/Committees.jsx
+++ src/pages/Committees.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Committees = () => {
+  const [committees, setcommittees] = useState([]);
+
+  useEffect(() => {
+    const fetchCommittess = async () => {
+      const respones = await axios.get("http://localhost:4000/api/committees");
+
+      setcommittees(respones.data.data);
+    };
+    fetchCommittess();
+  }, []);
+
+  const deleteComittee = async (_id) => {
+    const respones = await axios.delete(
+      `http://localhost:4000/api/features/${_id}`
+    );
+    console.log(respones);
+  };
   return (
     <div className="max-w-screen-xl mx-auto px-4 md:px-8">
       <div className="items-start justify-between md:flex">
         <div className="max-w-lg">
           <h3 className="text-gray-800 text-xl font-bold sm:text-2xl">
-            Team members
+            Committees
           </h3>
           <p className="text-gray-600 mt-2">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry.
           </p>
         </div>
-        <div className="mt-3 md:mt-0">
+        <div className="mt-3">
           <a
             href="/"
             className="inline-block px-4 py-2 text-white duration-150 font-medium bg-indigo-600 rounded-lg hover:bg-indigo-500 active:bg-indigo-700 md:text-sm"
           >
-            Add member
+            Add Committees
           </a>
         </div>
       </div>
@@ -26,36 +44,34 @@ const Committees = () => {
         <table className="w-full table-auto text-sm text-left">
           <thead className="bg-gray-50 text-gray-600 font-medium border-b">
             <tr>
-              <th className="py-3 px-6">Username</th>
-              <th className="py-3 px-6">Email</th>
+              <th className="py-3 px-6">Name</th>
+              <th className="py-3 px-6">Image</th>
               <th className="py-3 px-6">Position</th>
-              <th className="py-3 px-6">Salary</th>
+              <th className="py-3 px-6">Description</th>
               <th className="py-3 px-6"></th>
             </tr>
           </thead>
           <tbody className="text-gray-600 divide-y">
-            {tableItems.map((item, idx) => (
+            {committees.map((Committee, idx) => (
               <tr key={idx}>
-                <td className="flex items-center gap-x-3 py-3 px-6 whitespace-nowrap">
-                  <img
-                    alt=""
-                    src={item.avatar}
-                    className="w-10 h-10 rounded-full"
-                  />
-                  <div>
-                    <span className="block text-gray-700 text-sm font-medium">
-                      {item.name}
-                    </span>
-                    <span className="block text-gray-700 text-xs">
-                      {item.email}
-                    </span>
-                  </div>
+                <td className="flCommittees-center gap-x-3 py-3 px-6 whitespace-nowrap">
+                  <span className="block text-gray-700 text-xs">
+                    {Committee.email}
+                  </span>
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
-                  {item.phone_nimber}
+                  <img
+                    alt=""
+                    src={Committee.avatar}
+                    className="w-10 h-10 rounded-full"
+                  />
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap">{item.position}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{item.salary}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {Committee.position}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  {Committee.salary}
+                </td>
                 <td className="text-right px-6 whitespace-nowrap">
                   <a
                     href="/"
@@ -64,7 +80,7 @@ const Committees = () => {
                     Edit
                   </a>
                   <button
-                    href="/"
+                    onClick={deleteComittee}
                     className="py-2 leading-none px-3 font-medium text-red-600 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg"
                   >
                     Delete
@@ -80,48 +96,3 @@ const Committees = () => {
 };
 
 export default Committees;
-
-const tableItems = [
-  {
-    avatar:
-      "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-    name: "Liam James",
-    email: "liamjames@example.com",
-    phone_nimber: "+1 (555) 000-000",
-    position: "Software engineer",
-    salary: "$100K",
-  },
-  {
-    avatar: "https://randomuser.me/api/portraits/men/86.jpg",
-    name: "Olivia Emma",
-    email: "oliviaemma@example.com",
-    phone_nimber: "+1 (555) 000-000",
-    position: "Product designer",
-    salary: "$90K",
-  },
-  {
-    avatar: "https://randomuser.me/api/portraits/women/79.jpg",
-    name: "William Benjamin",
-    email: "william.benjamin@example.com",
-    phone_nimber: "+1 (555) 000-000",
-    position: "Front-end developer",
-    salary: "$80K",
-  },
-  {
-    avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
-    name: "Henry Theodore",
-    email: "henrytheodore@example.com",
-    phone_nimber: "+1 (555) 000-000",
-    position: "Laravel engineer",
-    salary: "$120K",
-  },
-  {
-    avatar:
-      "https://images.unsplash.com/photo-1439911767590-c724b615299d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-    name: "Amelia Elijah",
-    email: "amelia.elijah@example.com",
-    phone_nimber: "+1 (555) 000-000",
-    position: "Open source manager",
-    salary: "$75K",
-  },
-];
